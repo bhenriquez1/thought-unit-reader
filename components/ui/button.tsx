7d7f915f2bd82dc6953d1e2477ca9fc712f0f46d
bhenriquez1 <avrrio14@gmail.com>
2025-05-17 05:19:31 +0000

    Fix: Update button.tsx with relative path to utils

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { cn } from "@/lib/utils"
+import { cn } from "../../lib/utils"
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
@@ -8,11 +8,8 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, ...props }, ref) => {
     return (
       <button
+        className={cn("px-4 py-2 rounded text-white bg-blue-600 hover:bg-blue-700", className)}
         ref={ref}
-        className={cn(
-          "inline-flex items-center justify-center rounded-md bg-primary px-4 py-2 text-sm font-medium text-white shadow transition-colors hover:bg-primary/90 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:opacity-50",
-          className
-        )}
         {...props}
       />
     )
