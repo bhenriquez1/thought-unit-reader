7107960891078aee2a1c414b695b1286c4b0a114
bhenriquez1 <avrrio14@gmail.com>
2025-05-17 06:01:24 +0000

    Fix: update utils import path to relative

--- components/ui/switch.tsx
+++ components/ui/switch.tsx
@@ -3,7 +3,7 @@
 import * as React from "react"
 import { Switch as HeadlessSwitch } from "@headlessui/react"
 import { cva } from "class-variance-authority"
-import { cn } from "@/lib/utils"
+import { cn } from "../../lib/utils"
 
 const switchVariants = cva(
   "peer inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out",
@@ -57,6 +57,7 @@ const Switch = React.forwardRef<HTMLButtonElement, SwitchProps>(
     )
   }
 )
+
 Switch.displayName = "Switch"
 
 export { Switch }
