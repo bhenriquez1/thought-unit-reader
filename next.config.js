b0f179b83a03a5ab13c29b7de577a7a4622a4400
bhenriquez1 <avrrio14@gmail.com>
2025-05-18 02:21:54 +0000

    Add next.config.js to fix react-pdf CSS import

---
+++ next.config.js
@@ -0,0 +1,12 @@
+// next.config.js
+const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader'],
+    });
+    return config;
+  },
+};
+
+module.exports = nextConfig;
