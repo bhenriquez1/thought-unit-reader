d54c0152026b9e2209e89cf121efbe833e15c848
bhenriquez1 <avrrio14@gmail.com>
2025-05-18 02:34:37 +0000

    ✅ Commit all changes: final parser integration, config fixes, and working build

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
-// next.config.js
 const nextConfig = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader'],
-    });
-    return config;
-  },
+  // Remove this webpack override unless you're doing something advanced
+  // webpack(config) {
+  //   config.module.rules.push({
+  //     test: /\.css$/,
+  //     use: ['style-loader', 'css-loader'],
+  //   });
+  //   return config;
+  // },
 };
 
 module.exports = nextConfig;
