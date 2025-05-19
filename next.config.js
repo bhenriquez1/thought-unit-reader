4e6d03b56c6c9e87843868ac751d4b9bf8f2bcae
bhenriquez1 <avrrio14@gmail.com>
2025-05-19 05:28:54 +0000

    ✅ Fix static export and ensure deployment from /out folder

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Remove this webpack override unless you're doing something advanced
-  // webpack(config) {
-  //   config.module.rules.push({
-  //     test: /\.css$/,
-  //     use: ['style-loader', 'css-loader'],
-  //   });
-  //   return config;
-  // },
+  output: "export",
+  reactStrictMode: true
 };
 
 module.exports = nextConfig;
