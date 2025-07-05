38dc06a48ca7c64dca853346c3f9e8aba2ba7725
bhenriquez1 <avrrio14@gmail.com>
2025-07-05 04:39:57 +0000

    Commit: pushed all latest changes

--- out/_next/static/ZoatnABfO9EbBLxIRURgF/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/353-0499454c32ed3b32.js","static/chunks/pages/index-bbe5773bbbd97de8.js"],"/_error":["static/chunks/pages/_error-56a1a2eec59e6cbd.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
