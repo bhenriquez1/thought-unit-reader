00ca6cff8686f0257cdfa06f65d84135c7c0131e
bhenriquez1 <avrrio14@gmail.com>
2025-07-22 01:43:29 +0000

    Restore Thought Unit Reader application and fix build issues

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    return config;
+  }
+}
+
+module.exports = nextConfig
