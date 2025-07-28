7a077237c91565e3c44ca1a2ff4397296bf67550
bhenriquez1 <avrrio14@gmail.com>
2025-07-28 23:41:02 +0000

    Update: commit all changes

--- components/HybridReader.tsx
+++ components/HybridReader.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState, useRef } from "react";
 import { Button } from "@/components/ui/button";
-import { Card } from "@/components/ui/card";
+import { Card } from "@/components/card";
 import { Input } from "@/components/ui/input";
 
 interface HybridReaderProps {
