3d92acc17ec40d2178525719ceecfb414e8cdb70
bhenriquez1 <avrrio14@gmail.com>
2025-07-29 00:00:54 +0000

    Update: commit all changes

--- components/HybridReader.tsx
+++ components/HybridReader.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState, useRef } from "react";
 import { Button } from "@/components/ui/button";
-import { Card } from "@/components/card";
+import { Card } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
