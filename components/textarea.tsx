6b03dff95a8358e982bcfd358a098b6717e51b91
bhenriquez1 <avrrio14@gmail.com>
2025-08-09 23:49:19 +0000

    Commit all changes

--- components/textarea.tsx
+++ components/textarea.tsx
@@ -1,25 +1,27 @@
 import * as React from "react";
-import { twMerge } from "tailwind-merge";
 
-export type TextareaProps = React.TextareaHTMLAttributes<HTMLTextAreaElement>;
+export interface TextareaProps
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
 
+/** Minimal textarea with sensible Tailwind defaults */
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
-  ({ className, ...props }, ref) => {
+  ({ className = "", ...props }, ref) => {
     return (
       <textarea
         ref={ref}
-        className={twMerge(
-          "w-full min-h-[120px] p-2 rounded border bg-gray-800 text-white",
-          "border-gray-600 placeholder:text-gray-400 outline-none",
-          "focus:border-gray-400 focus:ring-1 focus:ring-gray-400",
+        className={
+          "w-full min-h-[120px] rounded border border-gray-300 bg-white px-3 py-2 text-sm text-black " +
+          "placeholder:text-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500 " +
+          "disabled:cursor-not-allowed disabled:opacity-50 " +
           className
-        )}
+        }
         {...props}
       />
     );
   }
 );
+
 Textarea.displayName = "Textarea";
 
-export { Textarea };
 export default Textarea;
+export { Textarea };
